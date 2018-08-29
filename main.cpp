f213d65eab3eb4350f8b70ffb5791d6551b5f27a
jphuc96 <jphuc96@gmail.com>
2018-08-29 23:17:52 +0700

    added new lib ModBuTCP

--- main.cpp
+++ main.cpp
@@ -1,14 +1,21 @@
 #include "mbed.h"
 #include "EthernetInterface.h"
+#include "ModBus-TCP.h"
 
 EthernetInterface eth;
+Modbus ModbusTCP;
 
 Serial pc(PA_9,PA_10,115200);
 
 int main()
 {
-    pc.printf("Modbus TCP test...\r\r\n");
+    pc.printf("=======================\r\r\n");
+    pc.printf("====mbed Modbus TCP====\r\r\n");
+    pc.printf("=======================\r\r\n");
+
     eth.connect();
-    pc.printf("IP: &s\r\r\n",eth.get_ip_address());
+
+    pc.printf("MAC: %s\r\r\n",eth.get_mac_address());
+    pc.printf("IP: %s\r\r\n",eth.get_ip_address());
 
 }
