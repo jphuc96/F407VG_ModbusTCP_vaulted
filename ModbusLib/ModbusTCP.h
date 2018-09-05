0d829a181c55bb75f0a383bd202809e41dfe0241
jphuc96 <jphuc96@gmail.com>
2018-09-05 23:24:38 +0700

    new modbus lib

---
+++ ModbusLib/ModbusTCP.h
@@ -0,0 +1,83 @@
+
+#include <Modbus.h>
+
+#ifndef __bswap_16
+ #define __bswap_16(num) ((uint16_t)num>>8) | ((uint16_t)num<<8)
+#endif
+
+#define MODBUSTCP_PORT 	  502
+#define MODBUSTCP_MAXFRAME 200
+#define MODBUSTCP_TIMEOUT 1000
+#define MODBUSTCP_UNIT	  255
+#define MODBUSTCP_MAX_TRANSACIONS 16
+#define MODBUSTCP_MAX_CLIENTS	  4
+
+// Callback function Type
+typedef bool (*cbModbusConnect)(IPAddress ip);
+
+typedef struct TTransaction TTransaction;
+
+typedef bool (*cbTransaction)(Modbus::ResultCode event, TTransaction* t);
+
+typedef struct TTransaction {
+	uint16_t	transactionId;
+	uint32_t	timestamp;
+	cbTransaction cb = nullptr;
+	uint8_t*	_frame;
+    bool operator ==(const TTransaction &obj) const
+	    {
+		    return transactionId == obj.transactionId;
+	    }
+};
+
+class ModbusTCP : public Modbus {
+	protected:
+	typedef union MBAP_t {
+		struct {
+			uint16_t transactionId;
+			uint16_t protocolId;
+			uint16_t length;
+			uint8_t	 unitId;
+		};
+		uint8_t  raw[7];
+	};
+    MBAP_t _MBAP;
+	cbModbusConnect cbConnect = nullptr;
+	cbModbusConnect cbDisconnect = nullptr;
+	WiFiServer* server = nullptr;
+	WiFiClient* client[MODBUSTCP_MAX_CLIENTS];
+	std::vector<TTransaction> _trans;
+	int16_t		transactionId = 0;  // Last started transaction. Increments on unsuccessful transaction start too.
+	int8_t n = -1;
+
+	TTransaction* searchTransaction(uint16_t id);
+	void cleanup(); 	// Free clients if not connected and remove timedout transactions
+	int8_t getFreeClient();    // Returns free slot position
+	int8_t getSlave(IPAddress ip);
+	bool send(IPAddress ip, cbTransaction cb);
+
+	public:
+	ModbusTCP();
+	uint16_t lastTransaction();
+	bool isTransaction(uint16_t id);
+	bool isConnected(IPAddress ip);
+	bool connect(IPAddress ip);
+	bool disconnect(IPAddress addr) {}  // Not implemented yet
+	void slave();
+	void master();
+	void task();
+	void begin(); 	// Depricated
+
+    void onConnect(cbModbusConnect cb = nullptr);
+	void onDisconnect(cbModbusConnect cb = nullptr);
+    IPAddress eventSource();
+
+    bool writeCoil(IPAddress ip, uint16_t offset, bool value, cbTransaction cb = nullptr);
+	bool writeHreg(IPAddress ip, uint16_t offset, uint16_t value, cbTransaction cb = nullptr);
+	bool pushCoil(IPAddress ip, uint16_t offset, uint16_t numregs = 1, cbTransaction cb = nullptr);
+	bool pullCoil(IPAddress ip, uint16_t offset, uint16_t numregs = 1, cbTransaction cb = nullptr);
+	bool pullIsts(IPAddress ip, uint16_t offset, uint16_t numregs = 1, cbTransaction cb = nullptr);
+	bool pushHreg(IPAddress ip, uint16_t offset, uint16_t numregs = 1, cbTransaction cb = nullptr);
+	bool pullHreg(IPAddress ip, uint16_t offset, uint16_t numregs = 1, cbTransaction cb = nullptr);
+	bool pullIreg(IPAddress ip, uint16_t offset, uint16_t numregs = 1, cbTransaction cb = nullptr);
+};
--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include "mbed.h"
 #include "EthernetInterface.h"
+#include "TCPServer.h"
+#include "TCPSocket.h"
 
 EthernetInterface eth;
-
+TCPServer srv;
+TCPSocket clt_sock;
+SocketAddress clt_addr;
 
 Serial pc(PA_9,PA_10,115200);
 
@@ -13,10 +17,23 @@ int main()
     pc.printf("=======================\r\r\n");
 
     eth.connect();
-
     pc.printf("MAC: %s\r\r\n",eth.get_mac_address());
     pc.printf("IP: %s\r\r\n",eth.get_ip_address());
 
-    
+    srv.open(&eth);
+    srv.bind(eth.get_ip_address(), 502);
+    srv.listen(5);
+
+    while (true) {
+        srv.accept(&clt_sock, &clt_addr);
+        pc.printf("accept %s:%d\n", clt_addr.get_ip_address(), clt_addr.get_port());
+        char buffer[100] = "Hello World! \r\r\nModbus TCP\r\r\n";
+        clt_sock.send(buffer,sizeof(buffer));
+        char rbuffer[64];
+        int rcount = clt_sock.recv(rbuffer, sizeof(rbuffer));
+        pc.printf("recv %s | %d \r\r\n",rbuffer,rcount);
+        delete[] rbuffer;
+    }
+
     return 1;
 }
