f9d7ab95e1c81bb38271b3a74d8abbc0c3a3306c
Hoang Phuc <jphuc96@gmail.com>
2018-09-14 01:07:29 +0700

    seem to work

--- main.cpp
+++ main.cpp
@@ -14,27 +14,43 @@ DigitalOut LedGreen(PD_14);
 DigitalOut LedYellow(PD_15);;
 
 Serial pc(PA_9,PA_10,115200);
-uint8_t buffer[64];
+// uint8_t buffer[64];
+
 int main()
 {
     LedBlue = 1;
     wait_ms(100);
     LedBlue = 0;
 
-    pc.printf("=======================\r\r\n");
-    pc.printf("====mbed Modbus TCP====\r\r\n");
-    pc.printf("=======================\r\r\n");
+    pc.printf("=======================\r\r\r\r\n");
+    pc.printf("====mbed Modbus TCP====\r\r\r\r\n");
+    pc.printf("=======================\r\r\r\r\n");
 
     eth.connect();
-    pc.printf("MAC: %s\r\r\n",eth.get_mac_address());
-    pc.printf("IP: %s\r\r\n",eth.get_ip_address());
+    pc.printf("MAC: %s\r\r\r\r\n",eth.get_mac_address());
+    pc.printf("IP: %s\r\r\r\r\n",eth.get_ip_address());
 
     MB.server_open(MODBUSTCP_PORT);
 
-    while (true)
-    {
-        pc.printf("Waiting...\r\r\n");
-        MB.server_run();
-    }
+    pc.printf("Running...\r\r\r\r\n");
+    MB.server_run();
+
     return 1;
 }
+
+
+// while (true)
+//     {
+//         int n = clt_sock.recv(buffer, sizeof(buffer));
+//         if( n < 0 )
+//         {
+//             clt_sock.close();
+//             srv.close();
+//         }
+//         if( n > 0 )
+//         {
+//             buffer[n] = '\0';
+//             printf("length : %d\r\r\r\n", n);
+//             clt_sock.send(buffer, n);
+//         }
+//     }
