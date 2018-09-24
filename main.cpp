d893d77ab37dcd853a389bb39022cac76fec489c
Hoang Phuc <jphuc96@gmail.com>
2018-09-24 23:42:36 +0700

    add modbus rtu master lib

--- main.cpp
+++ main.cpp
@@ -1,13 +1,11 @@
 #include "mbed.h"
 #include "EthernetInterface.h"
-#include "TCPServer.h"
-#include "TCPSocket.h"
-#include "Modbus.h"
+
 #include "Modbus-TCP.h"
+#include "ModbusMaster.h"
 
 EthernetInterface eth;
-// ModbusTCP MB(&eth);
-ModbusTCP modbustcp;
+ModbusTCP modbustcp_s;
 
 DigitalOut LedRed(PD_12);
 DigitalOut LedBlue(PD_13);
@@ -15,11 +13,15 @@ DigitalOut LedGreen(PD_14);
 DigitalOut LedYellow(PD_15);
 
 Serial pc(PA_9,PA_10,115200);
-// uint8_t buffer[64];
+
+BufferedSerial buffered_serial(PD_5,PA_3);
+ModbusMaster modbusrtu_m_s1, modbusrtu_m_s2;
 
 /*Thread declares*/
+Thread thread_modbusrtu_routine(osPriorityNormal,1024,NULL,"modbusrtu_routine");
 Thread thread_update_registers(osPriorityNormal,OS_STACK_SIZE,NULL,"update_registers");
 void task_update_registers();
+void task_modbusrtu_routine();
 
 int main()
 {
@@ -27,29 +29,34 @@ int main()
     wait_ms(100);
     LedBlue = 0;
 
-    pc.printf("=======================\r\r\n");
-    pc.printf("====mbed Modbus TCP====\r\r\n");
-    pc.printf("=======================\r\r\n");
+    pc.printf("===========================\r\r\n");
+    pc.printf("====mbed Modbus TCP-RTU====\r\r\n");
+    pc.printf("===========================\r\r\n");
 
     eth.connect();
     pc.printf("MAC: %s\r\r\n",eth.get_mac_address());
     pc.printf("IP: %s\r\r\n",eth.get_ip_address());
 
-    modbustcp.addHreg(0,100);
-    modbustcp.addHreg(1,200);
-    modbustcp.addHreg(2,300);
-    modbustcp.addHreg(3,400);
+    buffered_serial.baud(57600);
+    buffered_serial.format(8,Serial::None,2);
+    pc.printf("Initialized Modbus RTU master.\r\r\n");
 
-    modbustcp.start(&eth,502);
-    pc.printf("ModbusTCP started\r\r\n");
+    modbustcp_s.start(&eth,502);
+    pc.printf("ModbusTCP server started\r\r\n");
+
+    modbustcp_s.addHreg(0,100);
+    modbustcp_s.addHreg(1,200);
+    modbustcp_s.addHreg(2,300);
+    modbustcp_s.addHreg(3,400);
 
     thread_update_registers.start(task_update_registers);
+    thread_modbusrtu_routine.start(task_modbusrtu_routine);
 
     while(1)
     {
-        LedGreen = 0;
+        // LedGreen = 0;
         Thread::wait(500);
-        LedGreen = 1;
+        // LedGreen = 1;
         Thread::wait(500);
     }
     return 1;
@@ -57,14 +64,40 @@ int main()
 
 void task_update_registers()
 {
-    //update register here, need to check Mutex
+    //update registers here, need to check Mutex
+    
+
+    AnalogIn cpu_temp(ADC_TEMP);
+    AnalogIn vbat(ADC_VBAT);
     for(;;)
     {
-        modbustcp.Hreg(0,rand());
-        modbustcp.Hreg(1,rand());
-        modbustcp.Hreg(2,rand());
-        modbustcp.Hreg(3,rand());
+        // modbustcp_s.Hreg(0,cpu_temp.read_u16());
+        // modbustcp_s.Hreg(1,vbat.read_u16());
+        // modbustcp_s.Hreg(2,rand());
+        // modbustcp_s.Hreg(3,rand());
 
         Thread::wait(500);
+    }
+}
+
+void task_modbusrtu_routine()
+{
+    uint8_t rd_result;
+    uint16_t MB_data;
+    modbusrtu_m_s1.begin(1,buffered_serial);
+    modbusrtu_m_s2.begin(2,buffered_serial);
+    for(;;)
+    {
+        rd_result = modbusrtu_m_s2.readHoldingRegisters(0x0000, 2);
+        if (rd_result == modbusrtu_m_s2.ku8MBSuccess)
+        {
+            modbustcp_s.Hreg(0, modbusrtu_m_s2.getResponseBuffer(0));
+            modbustcp_s.Hreg(1, modbusrtu_m_s2.getResponseBuffer(1));
+        }
+        else
+        {
+            //nothing
+        }  
+        Thread::wait(1000);
     }
 }
